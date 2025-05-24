9f9c3b25c973b7baa307a49161f1b0eff0dad018
PODTECH1 <pordtech6@gmail.com>
2025-05-24 16:14:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pordtech.info</title>
+    <link rel="icon" href="pordtech.jpg" class="">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+     integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+      crossorigin="anonymous" 
+    referrerpolicy="no-referrer" />
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="images.css">
+</head>
+<body>
+    <nav>
+        <a href="" class="logo">
+            <img src="pordtech.jpg" alt="">PORDTECH</a>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#services">Services </a></li>
+            <li><a href="#works">Works</a></li>
+            <li><a href="#location">Locations</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+    <form action="connection.php" class=""></form>
+    <div class="bars-animation">
+        <div class="bar" style="--i:6;"></div>
+        <div class="bar" style="--i:5;"></div>
+        <div class="bar" style="--i:4;"></div>
+        <div class="bar" style="--i:3;"></div>
+        <div class="bar" style="--i:2;"></div>
+        <div class="bar" style="--i:1;"></div>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+    <section class="home" id="home">
+        <div class="home-info">
+            <h1>Leopord Salum</h1>
+            <h2>I'm a 
+                <span style="--i:4;" data-text="Frontend 
+                Developer">Frontend Developer</span>
+                <span style="--i:3;" data-text="Commercial 
+                Teacher">Commercial Teacher</span>
+                <span style="--i:2;" data-text="Graphics Designer"
+                >Graphics Designer</span>
+                <span style="--i:1;" data-text="Financial
+                Consultant">Financial Consultant</span>
+            </h2>
+            <p>We are better in coding various websites like in Ecommerce,Shops,School,<br>
+            which allows your business to be in networks Also,graphics designing<br>so that many people can access<br>
+        </p>
+        <div class="btn-sci">
+            <a href="PORD CV.pdf." class="btn"> Download CV</a>
+            <div class="sci">
+                <a href="" target="_blank">
+                    <i class="fa-brands fa-facebook"></i></a>
+                    <a href="https://instagram.com/@pord tech" target="_blank">
+                        <i class="fa-brands fa-instagram"></i></a>
+                <a href="https://api.whatsapp.com/send?
+                phone=0763145584&text" class="float" target="_blank">
+                    <i class="fa-3x fa-brands fa-whatsapp"></i></a>
+                    <a href="https://linkedin.com/leopord salum" class="float" target="_blank">
+                        <i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </div>
+     </div>
+     <div class="home-img">
+        <div class="img-box">
+            <div class="img-item">
+                <img src="images/about.JPG" width="300" height="300" class="profile">
+            </div>
+        </div>
+     </div>
+    </section>
+</--Our Services--/>
+<div class="section" id="services">
+    <div class="headig">
+    <div class="title">
+        <h1>Our Services</h1>
+    </div>
+    <div class="wrapper">
+       <div class="content-box">
+        <div class="card">
+            <h2>Online Services</h2>
+            <p> Web and apps development<br> 
+               Loan Application <br>TIN application<br>
+               RITA verification<br>BRELA licence application<br>University Applications
+           </p>
+        </div>
+        <div class="card">
+            <h2>Window Installation</h2>
+            <p>
+                computer issues that occured we here to<br>
+                 solve that kind of problem because our <br>
+                company is the one which listen customer in high rate 
+            </p>
+        </div>
+        <div class="card">
+            <h2>Teaching Business Education</h2>
+            <p> Also from our company we have teacher who teach commercial<br> 
+               subjects includes commerce and bookkeeping at bring business ideas<br>
+           </p>
+        </div>
+        <div class="card">
+            <h2>Microsoft Office Installation</h2>
+            <p>  Our Company it ensure that you<br>
+                 computer is still function<br>
+                 like excell,word,power point <br>
+                welcome to us </p>
+        </div>
+        <div class="card">
+            <h2>Graphics Design</h2>
+            <p> we deal with digital marketing<br>
+                posters,business profiles, which can invite customers <br>
+               company is the one which listen customer in high rate 
+           </p>
+        </div>
+        <div class="card">
+            <h2>Financial Analysis</h2>
+            <p> 
+                I specialize in Financial Planning,investment analysis<br>
+                ,risk management and performance tracking
+                ,providing data driven insights to help individuals<br>
+                 and  businesses to achieve financial goals
+           </p>
+        </div>
+        <div class="card">
+            <h2>Tax Returns Updates</h2>
+            <p> we deal with digital marketing<br>
+                posters,business profiles, which can invite customers <br>
+               company is the one which listen customer in high rate 
+           </p>
+        </div>
+        <div class="card">
+            <h2>Graphics Design</h2>
+            <p> we deal with digital marketing<br>
+                posters,business profiles, which can invite customers <br>
+               company is the one which listen customer in high rate 
+           </p>
+        </div>
+       </div>
+       </div>
+       </div>
+   <section class="works" id="works">
+    <div class="heading">
+        <h1 class="h">My work</h1>  
+    </div>
+  <div class="row">
+      <div class="class-col">
+          <img src="cdn.jfif " width="100%" height="100%">
+          <div class="layer">
+              <h3>Web development</h3>
+          </div>
+          </div>
+          <div class="class-col">
+              <img src="images/images (5).jfif">
+              <div class="layer">
+                  <h3>Mobile applications</h3>
+              </div>
+          </div>
+              <div class="class-col">
+                  <img src="FINAL.jpeg" width="100%" height="100%">
+                  <div class="layer">
+                      <h3>Commercial Teaching</h3>
+              </div>
+      </div>
+  </div>
+ </section>
+ <section class="about" id="about">
+    <section class="header">
+    <div class="heading">
+        <h1 class="h">About Us</h1>  
+      </div>
+      <div class="container">
+          <div class="hero-content">
+              <h2>We are transform your business to digital</h2>
+              <p>Our company started since 2024 under CEO,LEOPORD ALLY SALUM
+                launched as small company of create websites which tend to
+                transform businesses to technology trends so that to increase and track
+                customers and ensure satisfactions within <br>and connect to AI<br> 
+                we are caring our customers aimed at expand our company all over 
+                country because we want to explore more opportunities
+              </p>               
+             </div>
+          <div class="hero-image">
+          </div>
+      </div>
+ </section>
+
+ <section class="contact" id="contact">
+    <div class="containe">
+       <div class="contact">
+          <h1>Contact Us</h1>
+          <form id="pord1" action="connection.php" method="POST">
+           <label for="name">your name</label>
+           <input type="text" class="input-field" name="name" required>
+           <label for="name">your email</label>
+           <input type="text" class="input-field" name="email" required>
+           <label for="message">message</label>
+           <textarea name="message" class="input-field"  required></textarea>
+           <button type="submit" class="submit-btn">send</button>
+          </form>
+       </div>
+       <div class="content">
+           <h1>Pord tech company</h1>
+           <p>welcome to our company dont fear to contact us<br> by using our information we have already provided<br> here for you</p>
+           <h2>Contact information</h2>
+           <p><strong>Email:</strong><a href="mailto:pordtech6@gmail.com">pordtech6@gmail.com</a></p>
+           <p><strong>Phone:</strong><a href="tel:+255 778 745 584">+255 778 745 584</a></p>
+           <p><strong>Address:</strong>255,kitunda,Dar-es-salaam,tanzania</p>
+ </section>
+
+ <div class="section" id="location">
+    <section class="head">
+    <div class="title">
+        <h1>Location</h1>
+    </div>
+    <div class="location">
+        <div class="">
+            <h2>KIPAWA-ILALA</h2>
+         <p> 
+            <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.2788349305692!2d39.20963507499591!3d-6.8571453931412805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c49c3834c91e5%3A0x6065bbf52f439356!2sIlala%20Secondary%20School!5e0!3m2!1sen!2stz!4v1726241088345!5m2!1sen!2stz" width="300" height="450" style="border:0;" allowfullscreen="" loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade"></iframe></p>
+        </p>
+        </div>
+        <div class="">
+            <h2>NDANDA-MTWARA</h2>
+         <p> 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31383.80135604174!2d39.0159702!3d-10.50262175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x18f2730784d03a7d%3A0x6be21ab3334b66d8!2sNdanda!5e0!3m2!1sen!2stz!4v1734958086820!5m2!1sen!2stz" width="300" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </p>
+        </div>
+        <div class="">
+            <h2>KITUNDA-DAR ES SALAM</h2>
+         <p> 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15843.549771719736!2d39.217172945070075!3d-6.904061496276888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c36883a0b9585%3A0x99c84dd077c13071!2sKitunda%20Kiyombo!5e0!3m2!1sen!2stz!4v1734958355817!5m2!1sen!2stz" width="300" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </p>
+        </div>
+    </div>
+</div>
+</div>
+ <section class="footer">
+    <div class="footer-content">
+        <a href="">PORD TECH</a>
+        <p class="">its new digital transformation
+        </p>
+    </div>
+        <div class="footer-content">
+            <h4 class="identity">Identity</h4>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#location">Our Locations</a></li>
+            <li><a href="#services">Our Services </a></li>
+            <li><a href="#contact">Contact</a></li>
+        </div>
+
+        <div class="footer-content">
+            <h4 class="contact" id="contact">Contact Us</h4>
+            <li><a href="tel:+255 778 745 584">+255 778 745 584</a></li>
+            <li><a href="mailto:pordtech6@gmail.com">pordtech6@gmail.com</a></li>
+        </div>
+        <div class="footer-content">
+            <h4 class="l" id="location">We are available</h4>
+            <li><a href="#location">kipawa</a></li>
+            <li><a href="#location">Ndanda</a></li>
+            <li><a href="#location">Kitunda</a></li>
+        </div>
+</section>
+<div class="footerBottom">
+<center> <p>Copyright &copy;2024 Designed by Pordtech company</p></center>
+</div>
+ </body>
+</html>
